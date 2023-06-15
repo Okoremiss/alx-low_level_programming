b0afc4e4dbecaafde9b5377b7bd61d0abac641cf
Okoremiss <whitneyokore21@gmail.com>
2023-06-15 15:19:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
-}	
+{       
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0); 
+}       
