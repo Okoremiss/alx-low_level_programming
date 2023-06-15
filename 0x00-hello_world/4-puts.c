f6d4dc0089400caddab265ed1f1988990533e0b0
Okoremiss <whitneyokore21@gmail.com>
2023-06-15 15:25:36 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
- *
- * Description: Prints a line of code using puts
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0); 
-}       
+{
+ 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
