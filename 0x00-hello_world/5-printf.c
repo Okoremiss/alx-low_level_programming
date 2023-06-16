9987bb8b8d860dcff6a9d00ecf39373c7e3ec9f7
Okoremiss <whitneyokore21@gmail.com>
2023-06-15 17:09:01 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,8 @@
 /**
  * main - Entry point
  * Description: Prints a line of code using printf
- * Return (0)
+ * Return: Always 0 (Success)
  */
-int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
