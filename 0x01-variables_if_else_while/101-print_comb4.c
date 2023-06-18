199c69dd4cbcece9cada9d46ac9b131925ec53a6
Okoremiss <whitneyokore21@gmail.com>
2023-06-18 04:50:59 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,10 +19,15 @@ int main(void)
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
+
+				if (num1 == 1 && num2 ==2 && num3 ==3)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
